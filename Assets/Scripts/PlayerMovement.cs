3e540b9e1713b0f723bac97ab1c0de9b12068a05
Guilherme Galarça Dutra <udutra@gmail.com>
2019-06-22 20:44:22 -0300

    Criação da Plataforma.
    Criação da Plataforma.
    Criação do Script do Player.
    Ajuste no Pixel per Unit do sprite Idle do Player.
    Adição do Sprite da flecha e do círculo.
    Ajustes da anti-aliasing.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera myCam;
+    private bool clicked;
+    public GameObject effects;
+    public Transform arrowHold, startPos;
+    public LineRenderer lineRenderer;
+    public float maxDistance = 7.5f;
+
+    private void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            effects.SetActive(true);
+            clicked = true;
+        }
+
+        if (clicked)
+        {
+            Vector2 mousePosition = myCam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = mousePosition - (Vector2) transform.position;
+
+            arrowHold.up = direction;
+
+            lineRenderer.SetPosition(0, startPos.position);
+
+            RaycastHit2D hit = Physics2D.Raycast(startPos.position, startPos.up, maxDistance);
+
+            if (hit.collider != null)
+            {
+                lineRenderer.SetPosition(1, hit.point);
+            }
+            else
+            {
+                lineRenderer.SetPosition(1, startPos.position + (startPos.up * maxDistance));
+            }
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                clicked = false;
+                effects.SetActive(false);
+            }
+        }
+    }
+}
